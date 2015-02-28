9ed5ab143c510933a4ccf6e2ab6db270e8614d8f
Benjamin Nygård <benjaladdin@hotmail.com>
2015-02-28 07:41:08 +0100

    dsfsd
    sfdfds

--- insertImg.php
+++ insertImg.php
@@ -8,6 +8,7 @@ if (isset($_SESSION['organizationNr'])) {
 
 	if ( $_FILES['file_background']['error'] > 0) {
 		echo 'Error: ' . $_FILES['file_background']['error'] . '<br>';
+		echo "HEEEELLO";
 	}
 	else {
 		$path = "Bilder/" . $organizationNr . "/Bakgrunnsbilde/";
@@ -25,134 +26,97 @@ if (isset($_SESSION['organizationNr'])) {
 
 
 
-		$uploadOk_bakgrunnsbilde = 1;
-		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+		move_uploaded_file($_FILES["file_background"]["tmp_name"], $target_file);
 
-		$check = getimagesize($_FILES["file_background"]["tmp_name"]);
-		if($check !== false) {
-			$uploadOk_bakgrunnsbilde = 1;
+		chmod($target_file, 0777);	
+
+		$backgroundimgURL = "http://localhost/sharityCRM/" . $target_file;
+		$sql = "UPDATE Organization SET backgroundimgURL = '$target_file' WHERE organizationNr = '$organizationNr'";
+
+		if (mysqli_query($connection, $sql)) {
+			echo "Successful MySQL queru INSERT";
 		} else {
-			$uploadOk_bakgrunnsbilde = 0;
+
+			die('Error: ' . mysqli_error($connection));
+			db_close($connection);
 		}
+	}
+}	
 
-		if (file_exists($target_file)) {
-			//echo "Sorry, file already exists.";
-			$uploadOk_bakgrunnsbilde = 0;
-		}
 
-		// Check file size
-		if ($_FILES["file_background"]["size"] > 500000) {
-			//echo "Sorry, your file is too large.";
-			$uploadOk_bakgrunnsbilde = 0;
-		}
 
-		// Allow certain file formats
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"&& $imageFileType != "gif" ) {
-			//echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-			$uploadOk_bakgrunnsbilde = 0;
-		}
 
-		if ($uploadOk_bakgrunnsbilde == 0) {
-			echo "Sorry, your file was not uploaded.";
 
-			// if everything is ok, try to upload file
-		}
-
-		else{
-
-			if (move_uploaded_file($_FILES["file_background"]["tmp_name"], $target_file)) {
-
-				chmod($target_file, 0777);	
-
-				$backgroundimgURL = "http://localhost/sharityCRM/" . $target_file;
-				$sql = "UPDATE Organization SET backgroundimgURL = '$target_file' WHERE organizationNr = '$organizationNr'";
-
-				if (mysqli_query($connection, $sql)) {
-					echo "Successful MySQL queru INSERT";
-				} else {
-
-					die('Error: ' . mysqli_error($connection));
-					db_close($connection);
-				}
-			}
-		}	
-
+if ( $_FILES['file_logo']['error'] > 0) {
+	echo 'Error: ' . $_FILES['file_logo']['error'] . '<br>';
+}
+else {
+	$path = "Bilder/" . $organizationNr . "/Logo/";
+	if (!file_exists($path)) {
+		mkdir($path, 0777, true);
 	}
 
-
-
-	if ( $_FILES['file_logo']['error'] > 0) {
-		echo 'Error: ' . $_FILES['file_logo']['error'] . '<br>';
-	}
-	else {
-		$path = "Bilder/" . $organizationNr . "/Logo/";
-		if (!file_exists($path)) {
-			mkdir($path, 0777, true);
-		}
-
-		chmod($path, 0777);
+	chmod($path, 0777);
 
 
 
-		$target_dir = $path;
-		$target_file = $target_dir . basename($_FILES["file_logo"]["name"]);
+	$target_dir = $path;
+	$target_file = $target_dir . basename($_FILES["file_logo"]["name"]);
 
 
 
 
+	$uploadOk = 1;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+	$check = getimagesize($_FILES["file_logo"]["tmp_name"]);
+	if($check !== false) {
 		$uploadOk = 1;
-		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	} else {
+		$uploadOk = 0;
+	}
 
-		$check = getimagesize($_FILES["file_logo"]["tmp_name"]);
-		if($check !== false) {
-			$uploadOk = 1;
-		} else {
-			$uploadOk = 0;
-		}
-
-		if (file_exists($target_file)) {
+	if (file_exists($target_file)) {
 			//echo "Sorry, file already exists.";
-			$uploadOk = 0;
-		}
+		$uploadOk = 0;
+	}
 
 		// Check file size
-		if ($_FILES["file_logo"]["size"] > 500000) {
+	if ($_FILES["file_logo"]["size"] > 500000) {
 			//echo "Sorry, your file is too large.";
-			$uploadOk = 0;
-		}
+		$uploadOk = 0;
+	}
 
 		// Allow certain file formats
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"&& $imageFileType != "gif" ) {
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"&& $imageFileType != "gif" ) {
 			//echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-			$uploadOk = 0;
-		}
+		$uploadOk = 0;
+	}
 
-		if ($uploadOk == 0) {
-			echo "Sorry, your file was not uploaded.";
+	if ($uploadOk == 0) {
+		echo "Sorry, your file was not uploaded.";
 
 			// if everything is ok, try to upload file
-		}
-
-		else{
-
-			if (move_uploaded_file($_FILES["file_logo"]["tmp_name"], $target_file)) {
-
-				chmod($target_file, 0777);	
-
-				$logoURL = "http://localhost/sharityCRM/" . $target_file;
-				$sql = "UPDATE Organization SET logoURL = '$target_file' WHERE organizationNr = '$organizationNr'";
-
-				if (mysqli_query($connection, $sql)) {
-					echo "Successful MySQL queru INSERT";
-				} else {
-
-					die('Error: ' . mysqli_error($connection));
-					db_close($connection);
-				}
-			}
-		}	
-
 	}
+
+	else{
+
+		if (move_uploaded_file($_FILES["file_logo"]["tmp_name"], $target_file)) {
+
+			chmod($target_file, 0777);	
+
+			$logoURL = "http://localhost/sharityCRM/" . $target_file;
+			$sql = "UPDATE Organization SET logoURL = '$target_file' WHERE organizationNr = '$organizationNr'";
+
+			if (mysqli_query($connection, $sql)) {
+				echo "Successful MySQL queru INSERT";
+			} else {
+
+				die('Error: ' . mysqli_error($connection));
+				db_close($connection);
+			}
+		}
+	}	
 
 }
 
