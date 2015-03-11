31e4647656a72f0e1a7bb76c74ed5f4bed046444
vegardlokreim93 <vegard.lokreim@icloud.com>
2015-03-12 00:30:29 +0100

    lagt til serverbackend for å hente organisasjoner fra database til app

--- phpBackend/getOrganizations.php
+++ phpBackend/getOrganizations.php
@@ -1,9 +1,9 @@
 <?php
-include "../phpBackend/connect.php";
-	//$sql = $_POST['sql'];
-	
-			//$sql = "SELECT title, txt FROM News WHERE projectID = 1";
-	$result = mysqli_query($connection, "SELECT * FROM Organization");
+
+include '../phpBackend/connect.php';
+if(isset($_POST['organizationSQL'])){
+	$sql = $_POST['organizationSQL'];
+	$result = mysqli_query($connection, $sql);
 	if (mysqli_num_rows($result) > 0) {
 		$rows = array();
 		while ($row = mysqli_fetch_assoc($result)) {
@@ -11,7 +11,9 @@ include "../phpBackend/connect.php";
 		}
 		echo json_encode($rows);
 	}else{
-		echo "not sukess";
+		echo "not success";
 	}
-//echo "Hello";
+}
+
 ?>
+
