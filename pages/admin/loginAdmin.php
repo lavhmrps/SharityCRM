d192dcdd793c28e937a6a488e261affaf1b24116
Benjamin Nygård <benjaladdin@hotmail.com>
2015-03-12 00:30:46 +0100

    la til link
    la til link i login admin, så man kommer seg til change_user. skal taes
    bort så fort login funker

--- pages/admin/loginAdmin.php
+++ pages/admin/loginAdmin.php
@@ -9,29 +9,30 @@
 </head>
 
 </body>
-	<div class="container">
+<div class="container">
 
 	<div class="col-md-3"></div>
-		<div class="col-md-6 text-center" id="adminlogin">
+	<div class="col-md-6 text-center" id="adminlogin">
 		<form>
-				<h1 style="color:white;">Admin</h1>
-				<input type="text" name="admin_username" class="form-control" id="username" placeholder="Username"/>
-				<input type="password" name="admin_password" class="form-control" id="passwd" placeholder="Password"/>
-				<input type="submit" name="login_admin" class="form-control" id="login_admin" value="Logg inn"/>
+			<h1 style="color:white;">Admin</h1>
+			<input type="text" name="admin_username" class="form-control" id="username" placeholder="Username"/>
+			<input type="password" name="admin_password" class="form-control" id="passwd" placeholder="Password"/>
+			<input type="submit" name="login_admin" class="form-control" id="login_admin" value="Logg inn"/>
+			<a href="change_user.php">Change user</a>
 
-			</form>
-		</div>
-		<div class="col-md-3"></div>
+		</form>
+	</div>
+	<div class="col-md-3"></div>
 
 
-		<!-- jQuery -->
-		<script src="js/jquery.js"></script>
-		<!--Check login information-->
-		<script src="checkLogin.js"></script>
-		<!-- Bootstrap Core JavaScript -->
-		<script src="../js/bootstrap.min.js"></script>
+	<!-- jQuery -->
+	<script src="js/jquery.js"></script>
+	<!--Check login information-->
+	<script src="checkLogin.js"></script>
+	<!-- Bootstrap Core JavaScript -->
+	<script src="../js/bootstrap.min.js"></script>
 
-		</body>
+</body>
 
 
 </html>
