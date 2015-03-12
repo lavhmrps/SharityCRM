149f09d89b1eecce9fac084b83d59538b0927a9e
vegardlokreim93 <vegard.lokreim@icloud.com>
2015-03-12 02:46:20 +0100

    dsadsadsa

--- js/checkLogin.js
+++ js/checkLogin.js
@@ -1,67 +1,70 @@
-$('#loginbutton').click(function(){
-	var organizationNr = $('input[name=organizationNr]').val();
-	var password = $('input[type=password]').val();
+$(document).ready(function(){
 
-	var combinationJSON = {
-		"organizationNr" : organizationNr,
-		"password" : password
-	};
+	$('#loginbutton').click(function(){
+		var organizationNr = $('input[name=organizationNr]').val();
+		var password = $('input[type=password]').val();
 
-	combinationJSON = JSON.stringify(combinationJSON);
+		var combinationJSON = {
+			"organizationNr" : organizationNr,
+			"password" : password
+		};
 
-	$.ajax({
-		type: "POST",
-		dataType: "text",
-		url: "phpBackend/checkLogin.php",
-		data: {'combination' : combinationJSON},
-		success: function(response){
-			if(response == "OK"){
-				window.location.replace("pages/registration_pt2.php");
-			}else if(response == "WRONG"){
-				alert("WRONG ORGANIZATION NUMBER / PASSWORD COMBINATION");
-				$('input[name=password]').val("");
-			}else if(response == "NULL"){
-				$('input[name=reg_organizationNr]').val(organizationNr);
-				$('input[name=reg_name]').focus();
-				$("#goToRegister").trigger("click");
+		combinationJSON = JSON.stringify(combinationJSON);
 
+		$.ajax({
+			type: "POST",
+			dataType: "text",
+			url: "phpBackend/checkLogin.php",
+			data: {'combination' : combinationJSON},
+			success: function(response){
+				if(response == "OK"){
+					window.location.replace("pages/registration_pt2.php");
+				}else if(response == "WRONG"){
+					alert("WRONG ORGANIZATION NUMBER / PASSWORD COMBINATION");
+					$('input[name=password]').val("");
+				}else if(response == "NULL"){
+					$('input[name=reg_organizationNr]').val(organizationNr);
+					$('input[name=reg_name]').focus();
+					$("#goToRegister").trigger("click");
+
+				}
+			},
+			error: function(response){
+				alert(response);
 			}
-		},
-		error: function(response){
-			alert(response);
-		}
+		});
+
+
+		return false;
 	});
 
+	$('input[name=login_admin]').click(function(){
+		var username = $('input[name=admin_username]').val();
+		var password = $('input[name=admin_password]').val();
+		
+		adminJSON = {	
+			"username" : username,
+			"password" : password 
+		};
 
-	return false;
-});
+		adminJSON = JSON.stringify(adminJSON);
 
-$('input[name=login_admin]').click(function(){
-	var username = $('input[name=admin_username]').val();
-	var password = $('input[name=admin_password]').val();
+		$.ajax({
+			type: "POST",
+			dataType: "text",
+			url: "../phpBackend/checkLogin.php",
+			data: {"adminCombination" : adminJSON},
+			success: function(response){
+				if(response == "OK"){
+					window.location.replace("../pages/admin/adminHome.php");
 
-	adminJSON = {
-		"username" : username,
-		"password" : password 
-	};
-
-	adminJSON = JSON.stringify(adminJSON);
-
-	$.ajax({
-		type: "POST",
-		dataType: "text",
-		url: "../phpBackend/checkLogin.php",
-		data: {"adminCombination" : adminJSON},
-		success: function(response){
-			if(response == "OK"){
-				window.location.replace("../pages/admin/adminHome.php");
-
+				}
+			},
+			error: function(response){
+				alert(response);
 			}
-		},
-		error: function(response){
-			alert(response);
-		}
-	});
+		});
 
-	return false;
+		return false;
+	});
 });
--- pages/admin/loginAdmin.php
+++ pages/admin/loginAdmin.php
@@ -6,32 +6,35 @@
 
 	<link href="../../css/bootstrap.min.css" rel="stylesheet">
 	<link href="../../css/Admin.css" rel="stylesheet">
+	<!-- jQuery -->
+	<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+	<!--Check login information-->
+	<script src="../js/checkLogin.js"></script>
+	<!-- Bootstrap Core JavaScript -->
+	<script src="../js/bootstrap.min.js"></script>
+
+	</script>
 </head>
 
 </body>
-	<div class="container">
+<div class="container">
 
 	<div class="col-md-3"></div>
-		<div class="col-md-6 text-center" id="adminlogin">
-		<form>
-				<h1 style="color:white;">Admin</h1>
-				<input type="text" name="admin_username" class="form-control" id="username" placeholder="Username"/>
-				<input type="password" name="admin_password" class="form-control" id="passwd" placeholder="Password"/>
-				<input type="submit" name="login_admin" class="form-control" id="login_admin" value="Logg inn"/>
+	<div class="col-md-6 text-center" id="adminlogin">
 
-			</form>
-		</div>
-		<div class="col-md-3"></div>
+		<h1 style="color:white;">Admin</h1>
+		<input type="text" name="admin_username" class="form-control" id="username" placeholder="Username"/>
+		<input type="password" name="admin_password" class="form-control" id="passwd" placeholder="Password"/>
+		<input type="submit" name="login_admin" class="form-control" id="login_admin" value="Logg inn"/>
+
+		
+	</div>
+	<div class="col-md-3"></div>
 
 
-		<!-- jQuery -->
-		<script src="js/jquery.js"></script>
-		<!--Check login information-->
-		<script src="checkLogin.js"></script>
-		<!-- Bootstrap Core JavaScript -->
-		<script src="../js/bootstrap.min.js"></script>
 
-		</body>
+
+</body>
 
 
 </html>
