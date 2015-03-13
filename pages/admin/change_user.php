3538da2338c0253f6e3da77bc1255e9545254ad3
pamoei <paal.morten@ntebb.no>
2015-03-13 14:19:10 +0100

    Oppdatering
    Oppdateret med riktig php-kode.

--- pages/admin/change_user.php
+++ pages/admin/change_user.php
@@ -35,7 +35,58 @@ include "adminHeader_nav.php";
 
 
 			<!-- Bare putt infoen som ble funnet ved søket i databasen inn her inntil videre.. så fikser jeg utseende senere. B :)-->
+			<?php
 
+				&email = $_GET['username'];
+
+				$sql = "SELECT * FROM User WHERE email = $email";
+				$result = mysqli_query($connection, $sql);
+
+				if($result){
+					if(mysqli_num_rows($result) == 1){
+						$row = mysqli_fetch_assoc($result);
+
+
+						$name = $row['name'];
+						$phone = $row['phone'];
+						$address = $row['address'];
+						$zip = $row['zip'];
+						$picURL = $row['picURL'];		
+						$password = $row['password'];
+
+
+						echo "<label>E-post</label>";
+						echo '<input type="email" class="form-control" name="email" id="changeUser" placeholder="" value="' . $email . '"/>';
+
+						echo "<label>Navn</label>";
+						echo '<input type="text" class="form-control" name="name" id="changeUser" placeholder="" value="' . $name . '"/>';
+
+						echo "<label>Telefonnummer/label>";
+						echo '<input type="tel" class="form-control" name="phone" id="changeUser" placeholder="" value="' . $phone . '"/>';
+
+						echo "<label>Adresse</label>";
+						echo '<input type="text" class="form-control" name="address" id="changeUser" placeholder="" value="' . $address . '"/>';
+
+						echo "<label>Postkode</label>";
+						echo '<input type="tel" class="form-control" name="zip" id="changeUser" placeholder="" value="' . $zip . '"/>';
+
+						echo "<label>Bilde URL</label>";
+						echo '<input type="text" class="form-control" name="picURL" id="changeUser" placeholder="" value="' . $picURL . '"/>';
+
+						echo "<label>Passord</label>";
+						echo '<input type="password" class="form-control" name="password" id="changeUser" placeholder="" value="' . $password . '"/>';
+
+						echo "<label>Gjenta passord</label>";
+						echo '<input type="password" class="form-control" name="repeat_password" id="changeUser" placeholder="" value="' . $password . '"/>';
+
+						echo '
+                            <button  class="btn btn-success" name="update_info">
+                            Oppdater bruker
+                            </button>
+                            ';
+					}
+				}
+			?>	
 
 
 		</div>
