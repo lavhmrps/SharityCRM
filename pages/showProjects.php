ee1a5950ec356009991126a31fc2559e83b176da
vegardlokreim93 <vegard.lokreim@icloud.com>
2015-03-16 20:53:15 +0100

    Upload image user

--- pages/showProjects.php
+++ pages/showProjects.php
@@ -60,7 +60,11 @@ $organizationNr = $_SESSION['organizationNr'];
 				echo '</div>';
 				echo "<div class='col-md-12' id='bottom'>";
 				echo '<a href="../pages/showSelectedProject.php" onclick="showProject(' . $row['projectID'] . ')">Vis</a> - ';
+<<<<<<< Updated upstream
 				echo '<a href="change_projectinfo.php" onclick="showProject(' . $row['projectID'] . ')">Endre</a> - ';
+=======
+				echo '<a href="change_projectinfo.php" onclick="showProject(' . $row['projectID'] . ')" >Endre</a> - ';
+>>>>>>> Stashed changes
 				echo '<a href="">Slett</a>';
 				echo '</div>';
 				echo '</div>';
