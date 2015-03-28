93548e5e1abaf7fd6ffc1baea31c781925823146
vegardlokreim93 <vegard.lokreim@icloud.com>
2015-03-28 02:52:17 +0100

    test til pål

--- phpBackend/test.php
+++ phpBackend/test.php
@@ -1,7 +1,19 @@
 <?php
 header('Access-Control-Allow-Origin: *'); 
+include '../phpBackend/connect.php';
 
 
-echo "Herrlo";
 
+if(isset($_POST['getSQL'])){
+	$sql = $_POST['getSQL'];
+	$result = mysqli_query($connection, $sql);
+	$json = Array();
+	if($result){
+		while($row = mysqli_fetch_assoc($result)){
+			$json[] = $row;
+		}
+	}
+	mysqli_close($connection);
+	echo json_encode($json);
+}
 ?>
