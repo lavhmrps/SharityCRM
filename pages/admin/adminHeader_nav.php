1e03c0e47090cdd8ba1a5c50723db0502a7d914a
Benjamin Nygård <benjaladdin@hotmail.com>
2015-03-28 03:19:26 +0100

    Admin update
    UPDATEA!

--- pages/admin/adminHeader_nav.php
+++ pages/admin/adminHeader_nav.php
@@ -1,25 +1,77 @@
 
-<?php
-
-if(!isset($_SESSION['username'])){
-		//header('Location: loginAdmin.php');
-
-		//<script type="text/javascript"> window.location adminHome;  </script>
-}
-if(isset($_POST["admin_logout"])){
-	session_unset();
-	session_destroy();
-
-	header('Location: loginAdmin.php');
-}
-	//include 'connect.php';
 
 
-?>
-<div class="col-md-1"></div>
-<div class="col-md-10 text-center">
-	<nav><a href="adminHome.php">Hjem</a> - <a href="change_organization.php">Endre organisasjon</a> - <a href="change_user.php">Endre bruker<a/> - <a href="statistics.php">Statitikk</a></nav>
+
+<nav class="navbar navbar-inverse navbar-static-top header">
+<br/>
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="adminHome.php">Sharity</a>
 </div>
-<div class="col-md-1">
-	<input type="submit" name = "admin_logout" value="logg ut"/>
-</div>
+
+<!-- Collect the nav links, forms, and other content for toggling -->
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav navbar-right">
+
+        <li>
+            <a href="adminHome.php">Hjem</a>
+        </li>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Organisasjon<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+                <li>
+                    <a href="changeOrg.php">Endre info</a>
+                </li>
+
+                <li>
+                    <a href="changeProject.php">Endre prosjekt</a>
+                </li>
+                <li>
+                    <a href="changeNews.php">Endre nyhet</a>
+                </li>
+            </ul>
+        </li>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Bruker<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+                <li>
+                    <a href="../pages/registerNews.php">Endre info</a>
+                </li>
+                 <li>
+                    <a href="../pages/showNews.php">Endre venner</a>
+                </li>
+                 <li>
+                    <a href="../pages/showNews.php">Endre abonnementer</a>
+                </li>
+                <li>
+                    <a href="../pages/showNews.php">Fjern donasjon</a>
+                </li>
+                 <li>
+                    <a href="../pages/showNews.php">Fjern kortinformasjon</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="../pages/newStatistics.php">Statistikk</a>
+        </li>
+        <li>
+            <a href="../pages/settings.php">Innstillinger</a>
+        </li>
+        <li>
+            <a href="#" name="loggUt">Logg ut</a>
+        </li>
+    </ul>
+</div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+<br/>
+</nav>
+
+<script type="text/javascript" src="../js/menuBar.js"></script>
+
--- pages/admin/adminHome.php
+++ pages/admin/adminHome.php
@@ -3,40 +3,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Admin</title>
-	
 
+
+	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Admin</title>
+
+	<!-- Bootstrap Core CSS -->
 	<link href="../../css/bootstrap.min.css" rel="stylesheet">
 	<link href="../../css/Admin.css" rel="stylesheet">
+	
+	
 </head>
 <body>
 
-	<div class="col-md-12 text-center"><h1>ADMIN</h1></div>
 	<?php
 	include "adminHeader_nav.php";
 	?>
 
+
 	<div class="container">
-		<div class="col-lg-1 col-md-0 col-xs-0"></div>
-
-		<div class="col-lg-10 col-md-12 col-xs-12">
-
-			<div class="col-lg-4 col-md-4 col-xs-12" onclick="location.href='showOrg.php';" id="homebox">
-				<h1>Organisasjon</h1>
-			</div>
-
-			<div class="col-lg-4 col-md-4 col-xs-12" onclick="location.href='showUser.php';" id="homebox">
-				<h1>Bruker</h1>
-			</div>
-
-			<div class="col-lg-4 col-md-4 col-xs-12" onclick="location.href='showStats.php';" id="homebox">
-				<h1>Statistikk</h1>
-			</div>
-
+		<div class="col-lg-2 col-md-0 col-xs-0"></div>
+		<div class="col-lg-8 col-md-12 col-xs-12 text-center" id="homebox">
+			<h2>Du er nå logget inn som administrator</h2>
 		</div>
-
-		<div class="col-lg-1 col-md-0 col-xs-0"></div>
+		<div class="col-lg-2 col-md-0 col-xs-0"></div>
 	</div>
+
+
+
+	<!-- jQuery -->
+	<script src="../../js/jquery.js"></script>
+	<script src="../../js/stickyheader.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="../../js/bootstrap.min.js"></script>
 
 </body>
 </html>
