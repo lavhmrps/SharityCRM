fc2956d6b0a3f3ead251e379f77cf1630f5531b4
Benjamin Nygård <benjaladdin@hotmail.com>
2015-04-10 19:59:47 +0200

    Admin - endre org
    Lagt til et søkefelt, som ikke har noe backend

--- pages/admin/changeOrg.php
+++ pages/admin/changeOrg.php
@@ -1,5 +1,5 @@
 <?php
-	include "../../phpBackend/connect.php";
+include "../../phpBackend/connect.php";
 ?>
 <!DOCTYPE html>
 <html>
@@ -35,11 +35,11 @@
 
 
 	<style type="text/css">
-	table, th, tr, td{
-		border: 1px solid black;
-		padding-left: 15px;
-		padding-right: 15px;
-	}
+		table, th, tr, td{
+			border: 1px solid black;
+			padding-left: 15px;
+			padding-right: 15px;
+		}
 	</style>
 
 	<script type="text/javascript">
@@ -47,59 +47,65 @@
 
 
 
-	function setOrgNr(organizationNr){
-		
-		$.ajax({
-			type: "POST",
-			data : {"organizationNr" : organizationNr},
-			url: "../../phpBackend/AdminSetSession.php",
-			success : function(response){
-				if(response == "OK"){
-					window.location.replace("changeOrganization.php");
+		function setOrgNr(organizationNr){
+
+			$.ajax({
+				type: "POST",
+				data : {"organizationNr" : organizationNr},
+				url: "../../phpBackend/AdminSetSession.php",
+				success : function(response){
+					if(response == "OK"){
+						window.location.replace("changeOrganization.php");
+					}
+				},
+				error : function(error){
+					console.log(JSON.stringify(error));
+					alert(error.message);
 				}
-			},
-			error : function(error){
-				console.log(JSON.stringify(error));
-				alert(error.message);
-			}
-		});
-	}
+			});
+		}
 
 	</script>
+	<div class="container">
 		
+
+
 		<div class="col-md-4"></div>
 		<div class="col-md-4">
-		<?php
-		
+			<div class="col-md-12 text-center">
+				<input type="text" name="search" placeholder="Søk.." />
+			</div>
+			<?php
+
 			echo '<table>
-				<tr>
-					<th>OrgNr</th>
-					<th>Navn</th>
-					<th>Funksjoner</th>
-				</tr>
-				';
+			<tr>
+				<th>OrgNr</th>
+				<th>Navn</th>
+				<th>Funksjoner</th>
+			</tr>
+			';
 
 
 
-				$db = mysqli_connect("localhost", "root", "", "database");
-				$sql = "SELECT * FROM organization";
-				$resultat = mysqli_query($db, $sql);
+			$db = mysqli_connect("localhost", "root", "", "database");
+			$sql = "SELECT * FROM organization";
+			$resultat = mysqli_query($db, $sql);
 
-				if($resultat){
-					if(mysqli_num_rows($resultat) > 0){
-						while($rad = mysqli_fetch_assoc($resultat)){
-							echo '</tr>';
-							echo '<td>'. $rad['organizationNr']. '</td>';
-							echo '<td>'. $rad['name']. '</td>';
-							echo '<td><a onclick="setOrgNr('. $rad['organizationNr'] .')" style="cursor:pointer;">Endre</a> - <a onclick="setOrgNr('. $rad['organizationNr'] .')" style="cursor:pointer;">Slett</a></td>';
-							echo '</tr>';
-						}
+			if($resultat){
+				if(mysqli_num_rows($resultat) > 0){
+					while($rad = mysqli_fetch_assoc($resultat)){
+						echo '</tr>';
+						echo '<td>'. $rad['organizationNr']. '</td>';
+						echo '<td>'. $rad['name']. '</td>';
+						echo '<td><a onclick="setOrgNr('. $rad['organizationNr'] .')" style="cursor:pointer;">Endre</a> - <a onclick="setOrgNr('. $rad['organizationNr'] .')" style="cursor:pointer;">Slett</a></td>';
+						echo '</tr>';
 					}
 				}
+			}
 
 			echo '</table>';
-		
-		?>
+
+			?>
 		</div>
 		<div class="col-md-4"></div>
 	</div>
@@ -108,12 +114,13 @@
 
 
 
-	<!-- jQuery -->
-	<script src="../../js/jquery.js"></script>
-	<script src="../../js/stickyheader.js"></script>
 
-	<!-- Bootstrap Core JavaScript -->
-	<script src="../../js/bootstrap.min.js"></script>
+<!-- jQuery -->
+<script src="../../js/jquery.js"></script>
+<script src="../../js/stickyheader.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="../../js/bootstrap.min.js"></script>
 
 </body>
 </html>
