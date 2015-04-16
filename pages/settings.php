8644be33227f08789372cdf4865d5fe4a23fa75a
Benjamin Nygård <benjaladdin@hotmail.com>
2015-04-16 18:12:11 +0200

    blabla
    ikke noe spesielt

--- pages/settings.php
+++ pages/settings.php
@@ -61,34 +61,31 @@ if(isset($_POST['registerNews'])){
 				<p>fikse font</p>
 
 				<p>ORGSIDEN:</p>
-				<p>Starte på statistikk</p>
-				<p>Vise antall nye faste givere og mistede givere</p>
-				<p>EKS: Faste givere: +42 denne mnd.</p>
+				<p>statistikk</p>
 				<p>Internal frames på statistikk? og gjøre de visible</p>
 				<p>Brødsmulesti</p>
 				<p>Alle script skal si "OK" om de blir kjørt</p>
-				<p>trimme loginstring før den sjekkes mot db</p>
 				<p>Vaske og trimme stringer før de sendes til db</p>
 				<p>Sjekke siden opp mot andre browsere</p>
 				<p>Fikse last opp bilde og logo knapp på endre org siden</p>
 				<p>Fikse bilde opplasting på reg nyhet og endre nyhet</p>
 				<p>Generelt fikse bildeopplastning slik at vi har tillatelse til å bruke bildene som blir lagt inn i db</p>
-				<p>Når man er inne på Vis eller endre nyhet går det ikke å trykke på prosjekt eller nyhet i menyen øverst.. FIKS</p>
 				<p>Vis alle nyheter til prosjekt-knapp når man er inne på vis prosjekt?</p>
-				<p>Change newsinfo funker det ikke å oppdatere.. </p>
+				<p>Change newsinfo funker det ikke å oppdatere.. legge til backend </p>
 				<p>Lage FAQ-side</p>
+				<br/>
+				
+
+			</div>
+			<div class="col-md-6 text-left">
+				<p>ADMINSIDEN:</p>
+				<p>Statistikk</p>
+				<p>Backend til alle sidene.</p>
 				<br/>
 				<p>APP:</p>
 				<p>Legge inn jquery lokalt i app</p>
 				<p>Push</p>
 				<p>Betalingsløsning</p>
-
-			</div>
-			<div class="col-md-6 text-left">
-				<p>ADMINSIDEN:</p>
-				<p>Fikse adminlogin</p>
-				<p>Statistikk</p>
-				<p>Backend til alle sidene.</p>
 				<br/>
 				<p>NICE TO HAVE GENERELT:</p>
 				<p>knapp for å ukryptere passord (login)</p>
