73d2070ce893600ec87bd8a31407382dee60452f
Vegard Lokreim <Vegard@Vegards-MacBook-Air.local>
2015-04-16 15:18:18 +0200

    fghjkløø

---
+++ pages/statistikkFølgere.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title></title>
+
+	<link href="../css/bootstrap.min.css" rel="stylesheet"/>
+	<link href="../css/datepicker.css" rel="stylesheet"/>
+
+	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script src="../js/Chart.min.js"></script>
+	<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.min.js"></script>
+	<script>
+	$(document).ready(function(){
+
+    });
+
+	</script>
+</head>
+<body>
+
+    <canvas id="statistikk"></canvas>
+	
+</body>
+</html>
